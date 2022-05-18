2a1988c4aba9e49f1f0043afa4a24a9967baa171
Dhanya <60929233+DHANYAV20@users.noreply.github.com>
2022-05-18 20:04:49 +0530

    Update images.py

--- images.py
+++ images.py
@@ -8,3 +8,7 @@ print(img.ndim)
 cv2.imshow("Chickencurryinabowl", img)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
+
+
+#https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
+#https://docs.python.org/3/library/time.html
